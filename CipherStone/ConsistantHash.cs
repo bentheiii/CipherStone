2255df7ca2a3403adb94da95ee208afffd49ced6
Ben <Ben@BEN-PC>
2016-08-26 11:30:40 +0300

    initial

---
+++ CipherStone/ConsistantHash.cs
@@ -0,0 +1,16 @@
+﻿namespace CipherStone
+{
+    public static class consistantHash
+    {
+        public static ulong ConsistantHash(string read)
+        {
+            ulong hashedValue = 3074457345618258791ul;
+            foreach (char t in read)
+            {
+                hashedValue += t;
+                hashedValue *= 3074457345618258799ul;
+            }
+            return hashedValue;
+        }
+    }
+}
